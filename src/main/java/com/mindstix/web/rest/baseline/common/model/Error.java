82ffbfd50507d7a9e8c6ab08a34db39cd933bcb0
Susmit Bansod <susmitb@vmware.com>
2018-07-09 11:21:01 +0530

    Merged 0.0.1-SNAPSHOT version into Master Adding code for rest baseline common artifact for version 0.0.1-SNAPSHOT 1. Adding Model classes for ApiResponse and Error 2. Adding required constants.
    Updated Group ID to reflect Mindstix owned domain.
    Updated the package structure.

---
+++ src/main/java/com/mindstix/web/rest/baseline/common/model/Error.java
@@ -0,0 +1,53 @@
+package com.mindstix.web.rest.baseline.common.model;
+
+/**
+ * 
+ * <code>Error</code> is a model class used to hold the error details.<br>
+ *
+ * @author Mindstix-ychawda, susmit
+ */
+public class Error {
+
+    /**
+     * Business Error code
+     */
+    private String error;
+
+    /**
+     * Business Error Message / Description
+     */
+    private String message;
+
+    /**
+     * This is a parameterized constructor.
+     * 
+     * @param error
+     * @param message
+     */
+    public Error(String error, String message) {
+        this.error = error;
+        this.message = message;
+    }
+
+    /**
+     * This is a default constructor.
+     */
+    public Error() {
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
